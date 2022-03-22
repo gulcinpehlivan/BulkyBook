09983d66616daf40f19715507a0986713a51d1e6
gulci <gulci@DESKTOP-N4UVRRH>
2022-03-22 13:42:11 +0300

    Add project files.

---
+++ BulkBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,119 @@
+﻿using BulkBookWeb.Data;
+using BulkBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+
+        public IActionResult Edit(int? id)  //id p.key'se Find kullanabiliriz değilse singleordefault ya da firstordefault!
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDB = _db.Categories.Find(id);
+            if (categoryFromDB == null)
+            {
+
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name ");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated succesfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GeT action method
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name ");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created succesfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)  //id p.key'se Find kullanabiliriz değilse singleordefault ya da firstordefault!
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDB = _db.Categories.Find(id);
+            if (categoryFromDB == null)
+            {
+
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if(obj==null)
+            {
+                return NotFound();
+            }
+
+
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+            TempData["success"] = "Category deleted succesfully!";
+            return RedirectToAction("Index");
+            
+            return View(obj);
+        }
+    }
+}
